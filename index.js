f313beb66cb25b15101a0923f6fb8b2ed0e42c6c
Relyful <13381270+Relyful@users.noreply.github.com>
2025-02-18 21:36:41 +0100

    Change http server to express server, need to add 3 remaining paths

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'), (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('File Sent!');      
+    }
+  })
+})
 
 const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  if (req.url === '/') {    
-    fs.readFile('./index.html', 'utf-8', (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "content-type": 'text/html' });
-      res.end(data);
-    });    
-  } else if (req.url === '/about') {
-    fs.readFile('./about.html', 'utf-8', (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "content-type": 'text/html' });
-      res.end(data);
-    });
-  } else if (req.url === '/contact-me') {
-    fs.readFile('./contact-me.html', 'utf-8', (err, data) => {
-      res.writeHead(200, { "content-type": 'text/html' });
-      res.end(data);
-    });
-  } else {
-    fs.readFile('./404.html', 'utf-8', (err, data) => {
-      res.writeHead(200, { "content-type": 'text/html' });
-      res.end(data);
-    });
-  };
-  
-});
+app.listen(PORT, () => {
+  console.log(`RelyServer listening on port ${PORT}!`);
+})
 
-server.listen(PORT, console.log(`Server running on ${PORT}`));
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
+//   if (req.url === '/') {    
+//     fs.readFile('./index.html', 'utf-8', (err, data) => {
+//       if (err) throw err;
+//       res.writeHead(200, { "content-type": 'text/html' });
+//       res.end(data);
+//     });    
+//   } else if (req.url === '/about') {
+//     fs.readFile('./about.html', 'utf-8', (err, data) => {
+//       if (err) throw err;
+//       res.writeHead(200, { "content-type": 'text/html' });
+//       res.end(data);
+//     });
+//   } else if (req.url === '/contact-me') {
+//     fs.readFile('./contact-me.html', 'utf-8', (err, data) => {
+//       res.writeHead(200, { "content-type": 'text/html' });
+//       res.end(data);
+//     });
+//   } else {
+//     fs.readFile('./404.html', 'utf-8', (err, data) => {
+//       res.writeHead(200, { "content-type": 'text/html' });
+//       res.end(data);
+//     });
+//   };
+  
+// });
+
+// server.listen(PORT, console.log(`Server running on ${PORT}`));
