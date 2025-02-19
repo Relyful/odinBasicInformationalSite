5433ef00f66bcb2d8dce8a8f91db903c38936b92
Relyful <13381270+Relyful@users.noreply.github.com>
2025-02-19 20:22:43 +0100

    Add additional missing routes

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
 // const http = require('http');
 // const fs = require('fs');
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'), (err) => {
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"), (err) => {
     if (err) {
       throw err;
     } else {
-      console.log('File Sent!');      
+      console.log("File Sent!");
     }
-  })
-})
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "./about.html"), (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("About Sent!");
+    }
+  });
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"), (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("Contact sent!");
+    }
+  });
+});
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`RelyServer listening on port ${PORT}!`);
-})
+});
 
 // const server = http.createServer((req, res) => {
 //   console.log(req.url);
-//   if (req.url === '/') {    
+//   if (req.url === '/') {
 //     fs.readFile('./index.html', 'utf-8', (err, data) => {
 //       if (err) throw err;
 //       res.writeHead(200, { "content-type": 'text/html' });
 //       res.end(data);
-//     });    
+//     });
 //   } else if (req.url === '/about') {
 //     fs.readFile('./about.html', 'utf-8', (err, data) => {
 //       if (err) throw err;
@@ -46,7 +66,7 @@ app.listen(PORT, () => {
 //       res.end(data);
 //     });
 //   };
-  
+
 // });
 
 // server.listen(PORT, console.log(`Server running on ${PORT}`));
